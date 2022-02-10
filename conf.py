eecc36fa72293e1214b2309fb14e9d7397c5fe99
saswat mund <79899982+saswat711@users.noreply.github.com>
2022-02-10 12:23:24 +0530

    key

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 """Configurations for telegram_alert.py"""
-API_KEY = "8523ae2a-9fe4-45b3-8839-1f06870a1744"               # This is your Bolt Cloud API Key
-DEVICE_ID = "BOLT8023930"                  # This is the device ID and will be similar to BOLTXXXX where XXXX is some number here shold = 300                     # Threshold beyond which the alert should be sent
+API_KEY = "8###########"               # This is your Bolt Cloud API Key
+DEVICE_ID = "BOLT#########"                  # This is the device ID and will be similar to BOLTXXXX where XXXX is some number here shold = 300                     # Threshold beyond which the alert should be sent
 FRAME_SIZE = 4
 MUL_FACTOR = 8
